1f46b649ac5f12d55d68f565f3280e1877c01665
rami davidov <ramusdavidoff@gmail.com>
2020-02-27 01:18:24 +0200

    compose email structure

--- js/apps/email-app/pages/email-app.cmp.js
+++ js/apps/email-app/pages/email-app.cmp.js
@@ -8,7 +8,8 @@ import emailSideFilter from '../cmps/email-side-filter.cmp.js'
 export default {
     template: `
     <section class="email-app">
-        <email-compose @click.native="composeEmail">
+        <email-compose @click.native="composeEmail"
+        :isCompose="isCompose">
         </email-compose>
         <email-side-filter @filtered="setSideFilter">
         </email-side-filter>

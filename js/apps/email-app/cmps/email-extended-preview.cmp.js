885a1a99d27b05852d939af83b927428ea8fce4d
rami davidov <ramusdavidoff@gmail.com>
2020-02-26 19:12:20 +0200

    added email-ext btn

--- js/apps/email-app/cmps/email-extended-preview.cmp.js
+++ js/apps/email-app/cmps/email-extended-preview.cmp.js
@@ -1,13 +1,20 @@
 export default {
     template: `
     <section class="email-extended-preview">
+
         <div class="email-extended-subject-row">
-            <h2>{{email.subject}}</h2>
+            <div class="email-extended-subject">
+                {{email.subject}}
+            </div>
+            <div>
+                <button class="remove-email-btn">X</button>
+                <button class="expand-email-btn">O</button>
+            </div>
         </div>
         <div class="email-extended-from">
             <h5>{{email.from}}</h5>
         </div>
-            <div class="email-extended-body">
+        <div class="email-extended-body">
         <h5>{{email.body}}</h5>
         </div>
     </section>

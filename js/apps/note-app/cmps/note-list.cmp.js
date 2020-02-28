4ae91d770f0b5af83822ca9f460154f87cca311d
rami davidov <ramusdavidoff@gmail.com>
2020-02-28 15:57:23 +0200

    displaying notes, starting CRUD: note-menu

--- js/apps/note-app/cmps/note-list.cmp.js
+++ js/apps/note-app/cmps/note-list.cmp.js
@@ -4,7 +4,6 @@ import notePreview from '../cmps/note-preview.cmp.js'
 export default {
     template: `
     <section class="note-list">
-        <h2>NoteList</h2>
         <note-preview v-for="note in notes"
         :note="note"
         :key="note.id">
--- js/apps/note-app/cmps/note-preview.cmp.js
+++ js/apps/note-app/cmps/note-preview.cmp.js
@@ -1,23 +1,30 @@
 // import { templateService } from '../template.service.js'
-import { noteText, noteImg } from '../template.service.js'
+import { noteText, noteImg, noteTodos, noteVideo } from '../template.service.js'
+import noteMenu from '../cmps/note-menu.cmp.js'
 
 export default {
     template: `
     <section class="note-preview">
         <component :is="note.type"
+                    :class="note.type"
                     :info="note.info">
         </component>
+        <note-menu :noteId="note.id">
+        </note-menu>
     </section>
     `,
     data() {
         return {
-       
+
 
         }
     },
     components: {
-        noteText, 
-        noteImg
+        noteMenu,
+        noteText,
+        noteImg,
+        noteTodos,
+        noteVideo,
     },
     props: ['note']
 }

8174a772a37d8766e9f447ddfc460043a38fb926
rami davidov <ramusdavidoff@gmail.com>
2020-02-29 22:03:29 +0200

    about page, team page, css fixes

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -1,9 +1,30 @@
 export default {
     template: `
     <section class="about-page">
-        <div class="about-page-main">
-            <h1>About US</h1>
+        <div class="about-page-hero">
+            <div class="about-page-main container">
+                <h1 class="about-title">The Appsus Story</h1>
+                <h2 class="about-subject">
+                Challenged by a new framework & working together as a team  for the first time. </br>
+                We will prevail.
+                </h2>
+                <h3 class="about-tech">
+                <i class="fab fa-js-square"></i>
+                <i class="fab fa-vuejs"></i>
+                <i class="fab fa-css3-alt"></i>
+                </h3>
+                </div>
         </div>
+        <div class="about-page-content container">
+            <p class="about-page-par">
+                We are proud to present our Coding Academy project for Sprint 3
+                Februar 2020
+            </p>
+            <router-link to="/about/team">
+            <button class="meet-team-btn" >Meet the team</button>
+            </router-link>
+        </div>
+        <router-view></router-view>
     </section>
     `
 }

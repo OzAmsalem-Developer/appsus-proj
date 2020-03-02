966ebd491a5c3b9ac8ca66cb7494478f41e7aa9b
rami davidov <ramusdavidoff@gmail.com>
2020-03-02 22:29:54 +0200

    homepage started

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,7 +1,23 @@
 export default {
     template: `
     <section class="home-page">
-        <h1>Appsus</h1>
+        <section class="hero">
+            <div class="hero-img">
+                <h1 class="hero-title">APPSUS</h1>
+                <button class="hero-cta-btn">Get started</button>
+            </div>
+        </section>
+        <div class="gallery-header">Appsus features</div>
+        <section class="gallery-apps">
+                <div class="gallery-card gallery-card-email">
+                </div>
+            <div class="gallery-card gallery-card-note"></div>
+            <div class="gallery-card gallery-card-book"></div>
+        </section>
     </section>
     `
 }
+
+
+/* <router-link to="/email" class="back-btn">
+</router-link> */

3ab8b256542b1bd13cd5ebc753cc7bcf77b7d4a5
rami davidov <ramusdavidoff@gmail.com>
2020-03-03 02:16:11 +0200

    about, team pages responsive

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -5,19 +5,19 @@ export default {
             <div class="about-page-main container">
                 <h1 class="about-title">The Appsus Story</h1>
                 <h2 class="about-subject">
-                Challenged by a new framework & working together as a team  for the first time. </br>
-                We will prevail.
+                    Challenged by a new framework & working together as a team  for the first time. </br>
+                    We will prevail.
                 </h2>
                 <h3 class="about-tech">
-                <i class="fab fa-js-square"></i>
-                <i class="fab fa-vuejs"></i>
-                <i class="fab fa-css3-alt"></i>
+                    <i class="fab fa-js-square"></i>
+                    <i class="fab fa-vuejs"></i>
+                    <i class="fab fa-css3-alt"></i>
                 </h3>
-                </div>
+            </div>
         </div>
         <div class="about-page-content container">
             <p class="about-page-par">
-                We are proud to present our Coding Academy project for Sprint 3
+                Coding Academy Sprint 3 project Apsus
                 Februar 2020
             </p>
             <router-link to="/about/team">
